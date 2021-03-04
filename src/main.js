3741da1b4ad1e019396895950b3d8dad78e7c96b
Ryan Miller <ryandmiller3521@gmail.com>
2021-03-04 15:46:15 -0700

    Added event listener to the game grid to make it clickable

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
+var spaces = document.querySelector("#boardGrid");
+
+spaces.addEventListener("click", checkSpace);
+
+
+
+
 var playerOne = new Player({id: "playerOne", token: "X"});
 var playerTwo = new Player({id: "playerTwo", token: "O"});
 
 var testGame = new Game(playerOne, playerTwo);
 
-testGame.updateGameBoard(0);
-testGame.updateGameBoard(1);
-testGame.updateGameBoard(2);
-testGame.updateGameBoard(3);
-testGame.updateGameBoard(4);
-testGame.updateGameBoard(5);
-testGame.updateGameBoard(6);
-testGame.updateGameBoard(7);
-testGame.updateGameBoard(8);
-
-
-testGame.updatePlayerWins();
-testGame.updatePlayerWins();
-console.log(testGame.playerOne.wins);
+function checkSpace(event) {
+  console.log(event.target);
+}
